4895b42d4634d8a221509d123d304d29ef9e0816
Vijay Tatavarthi <tatavarthiv@gmail.com>
2025-07-23 00:58:35 -0700

    custom upload button + shadui init

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,27 +1,19 @@
 "use client";
 
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
-import { UploadButton } from "~/utils/uploadthing";
+import { SimpleUploadButton } from "./simple-upload-button";
 
 export default function TopNav() {
-  const router = useRouter();
-
   return (
     <nav className="flex items-center justify-between border-b border-white p-4">
       <h1 className="text-2xl font-bold">Image Gallery</h1>
 
-      <div className="flex flex-row gap-4">
+      <div className="flex flex-row items-center gap-4">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
-          <UploadButton
-            endpoint="imageUploader"
-            onClientUploadComplete={() => {
-              router.refresh();
-            }}
-          />
+          <SimpleUploadButton />
           <UserButton />
         </SignedIn>
       </div>
