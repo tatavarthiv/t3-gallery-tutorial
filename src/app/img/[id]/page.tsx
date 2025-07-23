75f6e701d64e9fc50714d15e996721eddbf10583
Vijay Tatavarthi <tatavarthiv@gmail.com>
2025-07-22 18:15:14 -0700

    modal cleanup

--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { notFound } from "next/navigation";
 import FullPageImageView from "~/components/full-image-page";
 
-export default async function PhotoModal({
+export default async function PhotoPage({
   params,
 }: {
   params: Promise<{ id: string }>;
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -2,5 +2,32 @@ import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
-  return <img src={image.url} className="w-96" alt={image.name} />;
+
+  return (
+    <div className="flex h-full w-screen min-w-0 items-center justify-center text-white">
+      <div className="flex-shrink flex-grow">
+        <img src={image.url} className="object-contain" alt={image.name} />
+      </div>
+      <div className="flex h-full w-56 flex-shrink-0 flex-col border-l">
+        <div className="border-b p-2 text-center text-xl">{image.name}</div>
+
+        <div className="p-2">
+          <div>Uploaded By:</div>
+        </div>
+
+        <div className="p-2">
+          <div>Created On:</div>
+          <div>{image.createdAt.toLocaleDateString()}</div>
+        </div>
+
+        <div className="p-2">
+          <form
+            action={async () => {
+              "use server";
+            }}
+          ></form>
+        </div>
+      </div>
+    </div>
+  );
 }
