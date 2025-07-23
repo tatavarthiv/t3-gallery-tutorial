f48f82704bd1bd1ba330a44b888470d8a8ea27c5
Vijay Tatavarthi <tatavarthiv@gmail.com>
2025-07-22 17:15:35 -0700

    parallel routes working finally 🤦

---
+++ src/app/@modal/(.)img/[id]/modal.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { type ElementRef, useEffect, useRef, useState } from "react";
+import { useRouter } from "next/navigation";
+import { createPortal } from "react-dom";
+
+export function Modal({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const dialogRef = useRef<ElementRef<"dialog">>(null);
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (!dialogRef.current?.open) {
+      dialogRef.current?.showModal();
+    }
+  }, []);
+
+  function onDismiss() {
+    router.back();
+  }
+
+  // Don't render anything during SSR
+  if (!isMounted) {
+    return null;
+  }
+
+  return createPortal(
+    <dialog
+      ref={dialogRef}
+      className="absolute h-screen w-screen bg-black/90"
+      onClose={onDismiss}
+    >
+      {children}
+      {/* <button onClick={onDismiss} className="close-button" /> */}
+    </dialog>,
+    document.getElementById("modal-root")!,
+  );
+}
