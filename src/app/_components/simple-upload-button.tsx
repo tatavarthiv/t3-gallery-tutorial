1e077611e20263aedd262326a433dafae87eb028
Vijay Tatavarthi <tatavarthiv@gmail.com>
2025-07-23 19:10:10 -0700

    upload event capture

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 import { useUploadThing } from "~/utils/uploadthing";
 import { toast } from "sonner";
+import { usePostHog } from "posthog-js/react";
 
 // inferred input off useUploadThing
 type Input = Parameters<typeof useUploadThing>;
@@ -72,9 +73,11 @@ function LoadingSpinnerSVG() {
 
 export function SimpleUploadButton() {
   const router = useRouter();
+  const posthog = usePostHog();
 
   const { inputProps } = useUploadThingInputProps("imageUploader", {
     onUploadBegin() {
+      posthog.capture("upload_begin");
       toast(
         <div className="flex items-center gap-2 text-white">
           <LoadingSpinnerSVG /> <span className="text-lg">Uploading...</span>
@@ -86,10 +89,12 @@ export function SimpleUploadButton() {
       );
     },
     onUploadError(error) {
+      posthog.capture("upload_error", { error });
       toast.dismiss("upload-begin");
       toast.error("Upload failed");
     },
     onClientUploadComplete() {
+      posthog.capture("upload_complete");
       toast.dismiss("upload-begin");
       toast("Upload complete!");
 
